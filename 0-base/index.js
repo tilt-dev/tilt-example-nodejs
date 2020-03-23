a87c2a304b7b7be326b93234b83b4381eb27a32f
Maia <maia.mcc@gmail.com>
2020-03-23 18:44:49 -0400

    base

---
+++ 0-base/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(8000, () => {
+    console.log('Server running at http://localhost:8000/');
+});
